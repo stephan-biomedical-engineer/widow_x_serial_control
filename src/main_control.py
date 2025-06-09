306c70c13bec9df1a835eb7e36855ceb10e32b14
stephan-biomedical-engineer <stephanbrrs8@gmail.com>
2025-06-09 10:57:48 -0300

    src att

--- src/main_control.py
+++ src/main_control.py
@@ -3,30 +3,40 @@
 #Posição inicial = [255, 5, 10, 0, 127, 0, 202, 0, 90, 2, 0, 1, 0, 125, 0, 0, 205]
 
 from ctrl_widow_x import WidowX as widow_x
+import time
 import serial as ser
 
 GO_SLEEP_CMD = [0xff, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x60, 0x9f]
+GO_HOME_CMD = [0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0xdf]
 
 def main():
     wx = widow_x()  # Cria uma instância da classe WidowX
     wx.connect()  # Conecta ao robô
     
     if wx.isConnected:
-        i = 0  # Inicializa a variável de controle do loop
+        y = 0  # Inicializa a variável de controle do loop
         try:
-            while i < 1:
-                gripper_position = gripper_value()  # Obtém o valor para o gripper
-                wx.sendValue(gripper=gripper_position)  # Envia o comando para o robô
-                if gripper_position == 0:
-                    i = 1  # Encerra o loop se o usuário não desejar continuar
-                    wx.comunicacaoSerial.write(GO_SLEEP_CMD)  # Envia o comando de inicialização
-                else:
-                    # Opcionalmente, você pode adicionar um delay aqui para evitar envio contínuo de comandos
-                    pass
+            while y < 1:
+                # gripper_position = gripper_value()  # Obtém o valor para o gripper
+                time.sleep(1)
+                for i in range(2000, 0, -1000):
+                    wx.sendValue(gripper=i)
+                    for j in range(0, 180, 30):
+                        print(f"Enviando valor {j} para o wrist.")
+                        time.sleep(0.5)
+                        wx.sendValue(wrist_rot = j)
+                        time.sleep(0.5)                
+                    print(f"Enviando valor {i} para o gripper.")
+                # time.sleep(2)
+                # wx.comunicacaoSerial.write(GO_HOME_CMD)  # Envia o comando de inicialização
+                # wx.goHome
+                time.sleep(5)
+                wx.comunicacaoSerial.write(GO_SLEEP_CMD)  # Envia o comando de inicialização
         except KeyboardInterrupt:
             wx.comunicacaoSerial.write(GO_SLEEP_CMD)  # Envia o comando de inicialização
             print("Programa encerrado pelo usuário.")
         finally:
+            wx.comunicacaoSerial.write(GO_SLEEP_CMD)  # Envia o comando de inicialização
             print("Fim do programa.")
 
 def gripper_value():
